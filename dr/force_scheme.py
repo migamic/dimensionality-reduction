47419adb31d5fc2b179c2ccd202c6768c27112a5
Jaume Ros Alonso <j.ros.alonso@tue.nl>
2025-02-10 18:06:23 +0100

    Remove explicit 2D code

--- dr/force_scheme.py
+++ dr/force_scheme.py
@@ -21,34 +21,6 @@ def create_distance_matrix(X, distance_function):
 
 
 @njit(parallel=True, fastmath=False)
-def move2D(ins1, distance_matrix, projection, learning_rate):
-    size = len(projection)
-    total = len(distance_matrix)
-    error = 0
-
-    for ins2 in prange(size):
-        if ins1 != ins2:
-            x1x2 = projection[ins2][0] - projection[ins1][0]
-            y1y2 = projection[ins2][1] - projection[ins1][1]
-            dr2 = max(math.sqrt(x1x2 * x1x2 + y1y2 * y1y2), 0.0001)
-
-            # getting te index in the distance matrix and getting the value
-            r = (ins1 + ins2 - math.fabs(ins1 - ins2)) / 2  # min(i,j)
-            s = (ins1 + ins2 + math.fabs(ins1 - ins2)) / 2  # max(i,j)
-            d_original = distance_matrix[int(total - ((size - r) * (size - r + 1) / 2) + (s - r))]
-
-            # calculate the movement
-            delta = (d_original - dr2)
-            error += math.fabs(delta)
-
-            # moving
-            projection[ins2][0] += learning_rate * delta * (x1x2 / dr2)
-            projection[ins2][1] += learning_rate * delta * (y1y2 / dr2)
-
-    return error / size
-
-
-@njit(parallel=True, fastmath=False)
 def move(ins1, distance_matrix, projection, learning_rate):
     size = len(projection)
     total = len(distance_matrix)
@@ -81,11 +53,7 @@ def iteration(index, distance_matrix, projection, learning_rate, n_components):
 
     for i in range(size):
         ins1 = index[i]
-
-        if n_components == 2:
-            error += move2D(ins1, distance_matrix, projection, learning_rate)
-        else:
-            error += move(ins1, distance_matrix, projection, learning_rate)
+        error += move(ins1, distance_matrix, projection, learning_rate)
 
     return error / size
 
