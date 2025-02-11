71e26ac4c17cf6e31cb8b30ea3cc69d58f02085d
Jaume Ros Alonso <j.ros.alonso@tue.nl>
2025-02-11 16:22:01 +0100

    Print CSV, and FS parameters

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ from dr.force_scheme import ForceScheme
 import metrics
 
 
+# Appends the line to the CSV file
+def export_csv(filename, line, append=True):
+    if filename is None:
+        return
+    if append:
+        with open(filename, "a") as f:
+            f.write(line+'\n')
+    else:
+        with open(filename, "w") as f:
+            f.write(line+'\n')
+
+
 # Returns a 2D symmetric array of size X*X
 def compute_distances(X):
     # squared_norms = np.sum(X**2, axis=1).reshape(-1, 1)
@@ -17,10 +29,9 @@ def compute_distances(X):
     return distance_matrix(X,X)
 
 
-def run_test(datasets, output_csv=False, plot=False):
-    if output_csv:
-        pass
+def run_test(datasets, params, output_csv=None, plot=False):
 
+    export_csv(output_csv, 'idx,dataset,n_pts,n_dim,iterations,max_iteration,time,stress,correlation', append=False)
     print('  idx |    dataset |   n_pts |   n_dim | iterations | time(s) |     stress |      corr.')
     print('---------------------------------------------------------------------------------------')
 
@@ -32,23 +43,24 @@ def run_test(datasets, output_csv=False, plot=False):
         X = preprocessing.StandardScaler().fit_transform(X)
 
         start = timer()
-        X_2D = ForceScheme(max_it=1).fit_transform(X)
+        X_2D, iters = ForceScheme(
+            max_it=params['max_it'],
+            learning_rate0=params['lr'],
+            decay=params['decay']
+        ).fit_transform(X)
         end = timer()
         elapsed_seconds = end-start
 
-        # TODO get this data from the FS run
-        iters = 10
-
         # Compute metrics
+        # TODO: maybe this is wrong. is D_high supposed to be a matrix? or a flattened vector?
         D_high = compute_distances(X)
         D_low = compute_distances(X_2D)
         stress = metrics.pq_normalized_stress(D_high, D_low)
         # sd_corr = metrics.pq_shepard_diagram_correlation(D_high, D_low) # TODO returns a matrix. How do I print this?
         sd_corr = 0
 
-        if output_csv:
-            pass
-        print(f"{i:>2}/{len(datasets):>2} | {data[:10]:>10} | {X.shape[0]:>7} | {X.shape[1]:>7} | {iters:>10} | {elapsed_seconds:>7.2f} | {stress:>10.4f} | {sd_corr:>10.4f}")
+        export_csv(output_csv, f'{i},{data},{X.shape[0]},{X.shape[1]},{iters},{10},{elapsed_seconds},{stress},{sd_corr}')
+        print(f"{str(i+1)+'/'+str(len(datasets)):>5} | {data[:10]:>10} | {X.shape[0]:>7} | {X.shape[1]:>7} | {str(iters)+'/'+str(params['max_it']):>10} | {elapsed_seconds:>7.2f} | {stress:>10.4f} | {sd_corr:>10.4f}")
 
         if plot:
             plt.figure()
@@ -58,11 +70,17 @@ def run_test(datasets, output_csv=False, plot=False):
 
 
 def main():
+    params = {
+        'max_it' : 200,
+        'lr'     : 0.5,
+        'decay'  : 0.8
+    }
+
     # imdb,sentiment have a similar structure as protein artifacts datasets
     big_data = ['cifar10', 'epileptic', 'hiva', 'imdb', 'spambase']
     small_data = ['har', 'orl', 'fmd', 'sms', 'svhn']
     datasets = ['bank', 'cifar10', 'cnae9', 'coil20', 'epileptic', 'fashion_mnist', 'fmd', 'har', 'hatespeech', 'hiva', 'imdb', 'orl', 'secom', 'seismic', 'sentiment', 'sms', 'spambase', 'svhn']
-    run_test(datasets, plot=False)
+    run_test(small_data, params, plot=False, output_csv=None)
 
 
 if __name__ == "__main__":
