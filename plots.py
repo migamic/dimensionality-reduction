d2005564667d93079d260d3422600fe8151c708e
Jaume Ros Alonso <j.ros.alonso@tue.nl>
2025-03-03 16:38:39 +0100

    Prettier plots

--- plots.py
+++ plots.py
@@ -6,7 +6,6 @@ df1 = pd.read_csv('csvs/default.csv')
 df2 = pd.read_csv('csvs/gradient.csv')
 df3 = pd.read_csv('csvs/scalable.csv')
 
-# Add a column identifying each setup
 df1['setup'] = 'Default'
 df2['setup'] = 'Gradient'
 df3['setup'] = 'Scalable'
@@ -14,15 +13,43 @@ df3['setup'] = 'Scalable'
 df = pd.concat([df1, df2, df3], ignore_index=True)
 datasets = df['dataset'].unique()
 
+sns.set(style="whitegrid", palette="Set2", rc={"axes.facecolor": "#f0f0f0", "grid.color": "#dcdcdc", "grid.linestyle": "--"})
+
 fig, axes = plt.subplots(nrows=1, ncols=len(datasets), figsize=(20, 6), sharey=True)
+
+boxplot_colors = sns.color_palette("Set2", n_colors=3)
 
 for i, dataset in enumerate(datasets):
     dataset_df = df[df['dataset'] == dataset]
     
-    sns.boxplot(x='setup', y='stress', data=dataset_df, ax=axes[i])
-    axes[i].set_title(f'{dataset}')
-    #axes[i].set_xlabel('Setup')
-    axes[i].set_ylabel('Stress')
+    sns.boxplot(
+        x='setup',
+        y='stress',
+        data=dataset_df,
+        ax=axes[i],
+        width=0.8,
+        linewidth=0.5,
+        flierprops=dict(markerfacecolor='red', marker='o', markersize=7),
+        hue='setup',
+        palette=boxplot_colors,
+        legend=False
+    )
 
+    axes[i].set_title(i+1, fontsize=12)
+    axes[i].set_xlabel(None)
+    axes[i].set_ylabel(None)
+    axes[i].set_xticklabels([])
+    axes[i].grid(True, linestyle='-', alpha=0.6, axis='y')
+    axes[i].tick_params(axis='y', labelsize=10)
+
+    for spine in axes[i].spines.values():
+        spine.set_visible(False)
+
+    max_stress = df['stress'].max()
+    axes[i].set_ylim(bottom=0, top=max_stress * 1.1)
+
+plt.subplots_adjust(wspace=0.1)
 plt.tight_layout()
+
 plt.show()
+
