911497b95e74f16e4858b0c6de3129306ee04e9c
Jaume Ros Alonso <j.ros.alonso@tue.nl>
2025-03-06 13:39:29 +0100

    More batch options

--- run_batch.py
+++ run_batch.py
@@ -1,51 +1,90 @@
 from test_fs import run_test
 
-def main():
-    datasets = ['bank', 'cifar10', 'cnae9', 'coil20', 'epileptic', 'fashion_mnist', 'fmd', 'har', 'hatespeech', 'hiva', 'imdb', 'orl', 'secom', 'seismic', 'sentiment', 'sms', 'spambase', 'svhn']
-    # datasets = ['orl', 'har']
 
-    default_FS = {
-        'max_it'     : 200,
+SETUPS = {
+    'FS': {
+        'max_it'     : 50,
         'lr'         : 0.1,
         'decay'      : 1,
         'rand_ord'   : False,
-        'err_win'    : 1,
+        'err_win'    : 0,
         'move_strat' : 'all',
         'n_anchors'  : 1,
         'normalize'  : True,
         'comp_dmat'  : True
-    }
+    },
 
-    gradient_FS = {
-        'max_it'     : 200,
-        'lr'         : 0.1,
-        'decay'      : 0.9,
-        'rand_ord'   : True,
-        'err_win'    : 5,
+    'FS_slow': {
+        'max_it'     : 500,
+        'lr'         : 0.01,
+        'decay'      : 1,
+        'rand_ord'   : False,
+        'err_win'    : 0,
         'move_strat' : 'all',
         'n_anchors'  : 1,
         'normalize'  : True,
         'comp_dmat'  : True
-    }
+    },
 
-    scalable_FS = {
+    'GFS': {
         'max_it'     : 200,
         'lr'         : 0.1,
         'decay'      : 0.9,
         'rand_ord'   : True,
-        'err_win'    : 5,
+        'err_win'    : 10,
+        'move_strat' : 'all',
+        'n_anchors'  : 1,
+        'normalize'  : True,
+        'comp_dmat'  : True
+    },
+
+    'SFS': {
+        'max_it'     : 200,
+        'lr'         : 0.1,
+        'decay'      : 0.9,
+        'rand_ord'   : True,
+        'err_win'    : 10,
         'move_strat' : 'sqrt',
         'n_anchors'  : 1,
         'normalize'  : False,
         'comp_dmat'  : False
     }
+}
 
-    seeds = list(range(3))
-    run_test(datasets, default_FS, output_csv='default', output_png='default', compute_metrics=True, seeds=seeds)
-    print()
-    run_test(datasets, gradient_FS, output_csv='gradient', output_png='gradient', compute_metrics=True, seeds=seeds)
-    print()
-    run_test(datasets, scalable_FS, output_csv='scalable', output_png='scalable', compute_metrics=True, seeds=seeds)
+
+def test_18_datasets():
+    datasets = ['bank', 'cifar10', 'cnae9', 'coil20', 'epileptic', 'fashion_mnist', 'fmd', 'har', 'hatespeech', 'hiva', 'imdb', 'orl', 'secom', 'seismic', 'sentiment', 'sms', 'spambase', 'svhn']
+    big_data = ['cifar10', 'epileptic', 'hiva', 'imdb', 'spambase']
+    small_data = ['orl', 'har', 'fmd', 'sms', 'svhn']
+    med_data = sorted(list(set(datasets)-set(big_data)-set(small_data)))
+    # datasets = med_data
+    # datasets = ['orl', 'har']
+
+    seeds = list(range(10))
+    for setup in ['FS', 'GFS', 'SFS']:
+        run_test(datasets, SETUP[setup], output_csv=setup, output_png=setup, compute_metrics=True, seeds=seeds)
+        print()
+
+
+def island_points_example():
+    datasets = ['cifar10', 'coil20', 'hatespeech','hiva', 'imdb']
+
+    for setup in ['FS', 'FS_slow', 'GFS', 'SFS']:
+        run_test(datasets, SETUPS[setup], output_png=setup+'islands', compute_metrics=True)
+        print()
+
+
+def big_dataset():
+    datasets = ['mammals50', 'fiber', 'fourier']
+
+    run_test(datasets, SETUPS['SFS'], show_plot=True, compute_metrics=False)
+
+
+def main():
+    # test_18_datasets()
+    # island_points_example()
+    big_dataset()
+
 
 
 if __name__ == "__main__":
